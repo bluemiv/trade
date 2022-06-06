b7b6edbc66bd2b09bb9f30601e64681a179a3a4f
TaehongKim <public.devhong@gmail.com>
2022-06-06 15:39:22 +0900

    feat: 분할매수 전략 추가

--- script/strategy/installment_purchase.py
+++ script/strategy/installment_purchase.py
@@ -9,30 +9,23 @@ from utils.deco import infinity_trade
 
 
 class InstallmentPurchase:
-    def __init__(self, init_krw=None, sell_rate=3, buy_rate=-5, black_list=[]):
+    def __init__(self, sell_rate=3, buy_rate=-5, black_list=[]):
         _config = config.get_config()
         self._upbit = UpbitHandler(_config['access_key'], _config['secret_key'])
-
-        self._init_krw = self._get_init_krw(init_krw)
-        print("매수 금액:", self._init_krw)
 
         self._sell_rate = sell_rate if sell_rate > 0 else sell_rate * -1
         self._buy_rate = buy_rate if buy_rate < 0 else buy_rate * -1
 
         self._black_list = black_list
 
-    def _get_init_krw(self, init_krw):
+    def _get_init_krw(self):
         """매수 금액을 설정한다"""
-        min_krw = 10000
-        max_krw = 50000
-        if init_krw is None:
-            result = int(self._upbit.get_my_total_krw() / 500)
-            if result < min_krw:
-                result = min_krw
-            if result > max_krw:
-                result = max_krw
-        else:
-            result = init_krw
+        min_krw, max_krw = (10000, 50000)
+        result = int(self._upbit.get_my_total_krw() / 500)
+        if result < min_krw:
+            result = min_krw
+        if result > max_krw:
+            result = max_krw
         return int(result)
 
     def log(self, currency, message):
@@ -41,6 +34,9 @@ class InstallmentPurchase:
 
     @infinity_trade(timer=8 * 60)
     def run(self):
+        self._init_krw = self._get_init_krw()
+        print("1회 매수 금액:", self._init_krw)
+
         # 현재 자산 조회
         my_currency_list = self._upbit.valid_currency_filter(self._upbit.get_my_currency_list())
         target_currency_list = self._upbit.get_target_currency(rate=6)
