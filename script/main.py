316af42ca81a79911ad891a195de286991af6960
TaehongKim <public.devhong@gmail.com>
2022-06-06 15:15:01 +0900

    feat: 분할 매수 전략 추가

--- script/main.py
+++ script/main.py
@@ -2,7 +2,28 @@
 
 from strategy.installment_purchase import InstallmentPurchase
 
+doc = '''
+ __   __             __   __  ___ 
+/  ` /  \ | |\ |    |__) /  \  |  
+\__, \__/ | | \|    |__) \__/  |  
+
+
+Strategy
+    1. 분할매수(installment parchase)
+'''
+
 if __name__ == '__main__':
+    print(doc)
+    while True:
+        strategy_number = input('Input strategy number: ')
+
+        if strategy_number == '1':
+            break
+        else:
+            print("Invalid input.")
+
     black_list = ['KRW-BTT', 'KRW-NU', 'KRW-LTC', 'KRW-XEM']
-    installmentPurchase = InstallmentPurchase(init_krw=10100, black_list=black_list)
-    installmentPurchase.run()
+
+    if strategy_number == '1':
+        installmentPurchase = InstallmentPurchase(init_krw=10100, black_list=black_list)
+        installmentPurchase.run()
