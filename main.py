2606c1ea8171f7f2084de82144c96ab40b134495
bluemiv <public.bluemiv@gmail.com>
2023-01-23 09:34:00 +0900

    feat: 로직 보완

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from strategy import infinity
 from strategy import rsi
 
 st = [
-    {"idx": 0, "label": "무한매매", "func": infinity.Infinity().run},
-    {"idx": 1, "label": "RSI 매매", "func": rsi.Rsi().run}
+    {"idx": 0, "label": "무한매매", "obj": infinity.Infinity},
+    {"idx": 1, "label": "RSI 매매", "obj": rsi.Rsi}
 ]
 
 if __name__ == '__main__':
     choice = '\n'.join(list(map(lambda x: f"{x['idx']}. {x['label']}", st)))
     num = input(f"매매 전략 선택\n{choice}\n > ")
-    st[int(num)]["func"]()
+    st[int(num)]["obj"]().run()
