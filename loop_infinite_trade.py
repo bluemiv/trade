1d0f4745a294a2601ab2e932cc6c24d0d8c1495e
taehongkim <public.bluemiv@gmail.com>
2025-01-15 15:05:36 +0900

    feat: 로직 보완

--- loop_infinite_trade.py
+++ loop_infinite_trade.py
@@ -3,21 +3,26 @@ import time
 from trade.infinit import TradeStrategy
 
 
-def loop_decorator(loop_time=2*60):
+def loop_decorator(loop_time=2 * 60):
     def decorator(func):
         def wrapper(*args, **kwargs):
             while True:
                 try:
                     func(*args, **kwargs)
+                    print(f"\nDelay {loop_time}\n")
                     time.sleep(loop_time)
                 except Exception as e:
                     print(f"ERROR. err: {e}")
+
         return wrapper
+
     return decorator
 
-@loop_decorator(loop_time=2*60) # 2분
+
+@loop_decorator(loop_time=2 * 60)  # 2분
 def main():
     TradeStrategy.infinit()
+
 
 if __name__ == "__main__":
     main()
